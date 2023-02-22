31c8142348f05a7188233df7e4907e2bc695b993
Patrick <patrick.plbarroso@gmail.com>
2023-02-22 11:49:54 -0300

    Bookly space until 5.url name

--- biblioteca/urls.py
+++ biblioteca/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from biblioteca.views import index
+from biblioteca.views import index, review
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', index),
+    path('', index, name='index'),
+    path('review/', review, name='review')
 ]
--- biblioteca/views.py
+++ biblioteca/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'biblioteca/index.html')
+
+def review(request):
+    return render(request, 'biblioteca/review.html')
