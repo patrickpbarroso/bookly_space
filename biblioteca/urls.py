246598bba79c6a58c392e0f6dd1aace2c61ba297
Patrick <patrick.plbarroso@gmail.com>
2023-03-08 21:11:37 -0300

    Configuring buscar.html

--- biblioteca/urls.py
+++ biblioteca/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from biblioteca.views import index, review
+from biblioteca.views import index, review, buscar
 
 urlpatterns = [
     path('', index, name='index'),
-    path('review/<int:resenha_id>', review, name='review')
+    path('review/<int:resenha_id>', review, name='review'),
+    path('buscar', buscar, name='buscar'),
 ]
--- biblioteca/views.py
+++ biblioteca/views.py
@@ -8,3 +8,12 @@ def index(request):
 def review(request, resenha_id):
     resenha = get_object_or_404(Resenha, pk=resenha_id)
     return render(request, 'biblioteca/review.html', {"resenha":resenha})
+
+def buscar(request):
+    resenhas = Resenha.objects.order_by("-data_postagem",).filter(publicada=True)
+
+    if "buscar" in request.GET:
+        nome_a_buscar = request.GET['buscar']
+        if nome_a_buscar: 
+            resenhas = resenhas.filter(titulo__icontains=nome_a_buscar)
+    return render(request, 'biblioteca/buscar.html', {"cards": resenhas})
